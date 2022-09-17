aa54d0770d70a69ac94c2c742067fc7435410858
jadashi97 <njadauniversal23@yahoo.com>
2022-09-16 21:17:16 -0500

    (feat): add content to other links (fix): solve the image issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import pic5 from "/Users/nyarjijada/Desktop/Personal-Main-Projects/church/src/pics/ss5.jpg";
 
 function Home(){
     return(
-        <div >
-            <div className="home-page">        
-                <h2>Our Mission</h2>
-                <div className="homely">
-                    <p>As a Community, we aim to glorify God by making disciples by reaching and teaching all people with the life-changing message of Jesus Christ.</p>
-                    <p>We believe that God's justifying grace must not be separated from His sanctifying power and purpose.</p>
-                    <p>God commands us to love Him supremely and others sacrificially, and to live out our faith with care for one another, 
-                    compassion toward the poor and justice for the oppressed. With God's Word, the Spirit's power, and fervent prayer in Christ's name, 
-                    we are to combat the spiritual forces of evil. </p>
-                    <p>In obedience to Christ's commission, we are to make disciples among all people, always bearing witness to the gospel in word and deed. </p>
-                </div>
+        <main > 
+            <div>
+                <img
+                    src={pic5}
+                    alt="home"
+                    // className="relative  flex justify-center object-scale-down object-top  ml-120 py-10 px-10 h-1000 w-800"
+                />
             </div>
-        </div>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-4">
+                <h2 className="text-1.5xl text-orange-400 font-bold cursive leading-none lg:leading-snug home-name">
+                    Our Mission
+                </h2>
+                <p>As a Community, we aim to glorify God by making disciples by reaching and teaching all people with the life-changing message of Jesus Christ.</p>
+                <p>We believe that God's justifying grace must not be separated from His sanctifying power and purpose.</p>
+                <p>God commands us to love Him supremely and others sacrificially, and to live out our faith with care for one another, 
+                compassion toward the poor and justice for the oppressed. With God's Word, the Spirit's power, and fervent prayer in Christ's name, 
+                we are to combat the spiritual forces of evil. </p>
+                <p>In obedience to Christ's commission, we are to make disciples among all people, always bearing witness to the gospel in word and deed. </p>              
+            </section>     
+        </main>
 
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,18 @@ function Navbar(){
     return(
         <header className="bg-yellow-600">
             <div className="container mx-auto flex justify-between">
+                    <strong className="inline-flex py-6 px-3 text-red-100 bg-red-700">
+                        South Sudanese <br />
+                        Community <br />
+                        Church
+                    </strong>
+
                 <nav className="flex">
+
                     <NavLink
                         to="Home"
                         activeclassname = "text-red-100 bg-red-700"
-                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold italic tracking-widest"
+                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-200 text-2xl font-bold italic tracking-widest"
                         style={({ isActive }) =>
                             isActive ? activeStyle : undefined
                         }
