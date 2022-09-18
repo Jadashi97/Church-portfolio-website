69e91818cbab8809f424ff917aac498ae6df5d2a
jadashi97 <njadauniversal23@yahoo.com>
2022-09-18 11:13:12 -0500

    (fix): figure out the styling of the pics and work on sanity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,23 +4,26 @@ import pic5 from "/Users/nyarjijada/Desktop/Personal-Main-Projects/church/src/pi
 function Home(){
     return(
         <main > 
-            <div>
+            <div className="bg-cyan-300 p-10 m-10">
                 <img
                     src={pic5}
                     alt="home"
-                    // className="relative  flex justify-center object-scale-down object-top  ml-120 py-10 px-10 h-1000 w-800"
+                    className="object-scale-down "
                 />
             </div>
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-4">
-                <h2 className="text-1.5xl text-orange-400 font-bold cursive leading-none lg:leading-snug home-name">
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-30 px-8">
+                <h2 className="text-1.0xl text-orange-400 font-bold cursive leading-none lg:leading-snug home-name">
                     Our Mission
                 </h2>
-                <p>As a Community, we aim to glorify God by making disciples by reaching and teaching all people with the life-changing message of Jesus Christ.</p>
-                <p>We believe that God's justifying grace must not be separated from His sanctifying power and purpose.</p>
-                <p>God commands us to love Him supremely and others sacrificially, and to live out our faith with care for one another, 
-                compassion toward the poor and justice for the oppressed. With God's Word, the Spirit's power, and fervent prayer in Christ's name, 
-                we are to combat the spiritual forces of evil. </p>
-                <p>In obedience to Christ's commission, we are to make disciples among all people, always bearing witness to the gospel in word and deed. </p>              
+                <div className="flex">
+                    <p>As a Community, we aim to glorify God by making disciples by reaching and teaching all people with the life-changing message of Jesus Christ.</p>
+                    <p>We believe that God's justifying grace must not be separated from His sanctifying power and purpose.</p>
+                    <p>God commands us to love Him supremely and others sacrificially, and to live out our faith with care for one another, 
+                        compassion toward the poor and justice for the oppressed. With God's Word, the Spirit's power, and fervent prayer in Christ's name, 
+                        we are to combat the spiritual forces of evil.
+                    </p>
+                    <p>In obedience to Christ's commission, we are to make disciples among all people, always bearing witness to the gospel in word and deed. </p>              
+                </div>
             </section>     
         </main>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      margin:{
+        "2px" : "2px",
+      }
+    },
   },
   plugins: [],
 }
