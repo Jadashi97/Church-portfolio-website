6ca94409d80ae446589258832a82751a840ccdcc
jadashi97 <njadauniversal23@yahoo.com>
2022-09-22 16:12:34 -0500

    (fix): solve why created SinglePost does not show

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../client";
 import { imageUrlBuilder } from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source){
@@ -39,22 +40,43 @@ export default function SinglePost(){
     if (!singlePost) return <div>Loading.......</div>
 
     return(
-        <main>
-            <article>
-                <header>
-                    <div>
-                        <div>
-                            <h1></h1>
-                            <div>
-                                <img src="" alt="" />
-                                <p></p>
+        <main className="bg-gray-200 min-h-screen p-12">
+            <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+                <header className="relative">
+                    <div className="absolute h-full w-full flex items-center justify-center p-8">
+                        <div className="bg-white bg-opacity-75 rounded p-12">
+                            <h1 className="cursive text-3xl lg:text-6xl mb-4">
+                                {singlePost.title}
+                            </h1>
+                            <div className="flex justify-center text-gray-800">
+                                <img 
+                                    src={urlFor(singlePost.authorImage).url()} 
+                                    alt={singlePost.name}
+                                    className="w-10 h-10 rounded-full"    
+                                />
+                                <p className="cursive flex items-center pl-2 text-2xl">
+                                    {singlePost.name}
+                                </p>
                             </div>
                         </div>
                     </div>
+                    <img 
+                        src={singlePost.mainImage.asset.url}
+                        alt={singlePost.title}
+                        className="w-full object-cover rounded-t"
+                        style={{height: "400px"}}
+                     />
+                
                 </header>
-                <div>BLOCK CONTENT</div>
+                <div className="px-16 lg:px-48 py-12 lg:py-20 prose-xl max-w-full">
+                    <BlockContent
+                        blocks={singlePost.body}
+                        projectId="zq00lcnz"
+                        dataset="production"
+                    />
+                </div>
             </article>
         </main>
-    )
+    );
     
 }
