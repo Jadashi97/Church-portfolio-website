72e0a48dcb4af92bc65440cc8f00857b0100c115
jadashi97 <njadauniversal23@yahoo.com>
2022-09-28 22:52:04 -0500

    (fix):refactored some styling and code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,14 @@ export default function Home() {
   return (
     <main className="bg-gray-100">
 
-      <div className=" justify-items-center">
-        <img
-          src={homePic}
-          alt="Monstera Leaves"
-          className="absolute object-cover py-20 px-20 w-full h-full"
-        />
+      <div>
+        <div className=" justify-items-center">
+          <img
+            src={homePic}
+            alt="Monstera Leaves"
+            className="w-200px h-200px ml-40 pt-10"
+          />
+        </div>
       </div>
       {/* <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
         <h1 className="text-4xl text-orange-600 font-bold cursive leading-loose home-name">
