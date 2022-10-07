d944cdb3a21efa66cd67a1de897f381e2e5ace2a
jadashi97 <njadauniversal23@yahoo.com>
2022-10-07 11:52:50 -0500

    (feat): added carousel and more styling to the website. (fix): fix responsive navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-import homePic from "../ss6.jpg";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import homePic1 from "../ss6.jpg";
+import homePic2 from "../ss2.jpg";
+import homePic3 from "../ss5.jpg";
 
 export default function Home() {
+   const images = [
+        homePic1,
+        homePic2,
+        homePic3,
+   ]
+    
+
   return (
     // <main className="bg-green-100 flex items-center ">
     //     <div className=" bg-green-100 prose">
@@ -12,37 +23,26 @@ export default function Home() {
     //       />
     //     </div>
     // </main>
-    <div className="relative w-[600px] mx-auto">
-      <div className="slide relative" >
-        <img
-          src={homePic}
-          alt="Monstera Leaves"
-          className="w-full h-[300px] object-cover"
-        />
-        <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white">Pic One Caption</div>
-      </div>
-      <div className="slide relative" >
-        <img
-          src={homePic}
-          alt="Monstera Leaves"
-          className="w-full h-[300px] object-cover"
-        />
-        <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white">Pic One Caption</div>
-      </div>
-      <div className="slide relative" >
-        <img
-          src={homePic}
-          alt="Monstera Leaves"
-          className="w-full h-[300px] object-cover"
-        />
-        <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white">Pic One Caption</div>
-      </div>
+    <div className="home-page h-full w-full bg-green-100 min-h-screen p-12">
 
-      {/* {<a class="absolute left-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-            onclick="moveSlide(-1)">❮</a>}
-
-        {<a class="absolute right-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-            onclick="moveSlide(1)">❯</a>} */}
+        <Slide className="bg-green-100 flex items-center">
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[0]})` }}>
+                    {/* <span>Slide 1</span> */}
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[1]})` }}>
+                    {/* <span>Slide 2</span> */}
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[2]})` }}>
+                    {/* <span>Slide 3</span> */}
+                </div>
+            </div>
+        </Slide>
     </div>
+
   );
 }
