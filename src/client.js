0d4e6574458152f2570e79414a52560abbd84dab
jadashi97 <njadauniversal23@yahoo.com>
2022-10-20 21:39:38 -0500

    (fix): solve sanity not showing projects and some data

--- src/client.js
+++ src/client.js
@@ -3,5 +3,7 @@ import  sanityClient  from "@sanity/client";
 
 export default sanityClient({
     projectId: "zq00lcnz",
-    dataset: "production"
+    dataset: "production",
+    apiVersion: '2021-08-31',
+    useCdn: true,
 })
